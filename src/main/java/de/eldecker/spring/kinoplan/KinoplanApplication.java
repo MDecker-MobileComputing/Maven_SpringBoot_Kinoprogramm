2da483728a2152d602cd972d996dd632b57fa6f5
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-06-28 17:39:54 +0200

    Projekt frisch erzeugt
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.5.3&packaging=jar&jvmVersion=21&groupId=de.eldecker.spring&artifactId=kinoplan&name=Kinoplan&description=Kinoplan%20mit%20Cassandra%20als%20Datenbank&packageName=de.eldecker.spring.kinoplan&dependencies=web,thymeleaf,data-cassandra

---
+++ src/main/java/de/eldecker/spring/kinoplan/KinoplanApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.spring.kinoplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinoplanApplication.class, args);
+	}
+
+}
